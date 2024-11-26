e3034f74e01e044e2b66d74cacb079539dc97a68
Camilo Villa <andresvillaz@gmail.com>
2024-11-26 17:19:02 -0500

    quitar exclusion

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+$host = '127.0.0.1';
+$usuario = 'root';
+$password = 'root';
+$base_datos = 'proyecto_mvc';
+$puerto = 3306;
+
+
+global $conn;
+$conn = new mysqli($host, $usuario, $password, $base_datos, $puerto);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+?>
